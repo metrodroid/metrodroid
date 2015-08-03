7d5bdf9fcb3fd8e98ce4b90af0d1e28548e15864
Michael Farrell <micolous+git@gmail.com>
2015-08-03 15:51:26 +1000

    bump version, remove debug keystore as this is now stored out of band

--- src/main/java/au/id/micolous/farebot/activity/MainActivity.java
+++ src/main/java/au/id/micolous/farebot/activity/MainActivity.java
@@ -26,6 +26,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
@@ -100,7 +101,19 @@ public class MainActivity extends Activity {
     @Override public boolean onOptionsItemSelected (MenuItem item) {
         switch (item.getItemId()) {
             case R.id.about:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://codebutler.github.com/farebot")));
+                Uri.Builder b = Uri.parse("https://micolous.github.io/farebot/").buildUpon();
+                int version = -1;
+                try {
+                    version = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+                } catch (PackageManager.NameNotFoundException ex) {
+                    // Shouldn't hit this...
+                }
+
+                // Pass the version number to the website.
+                // This allows the website to have a hook showing if the user's version is out of date
+                // and flag specifically which cards *won't* be supported (or have problems).
+                b.appendQueryParameter("ver", Integer.toString(version));
+                startActivity(new Intent(Intent.ACTION_VIEW, b.build()));
                 return true;
             case R.id.prefs:
                 startActivity(new Intent(this, FareBotPreferenceActivity.class));
