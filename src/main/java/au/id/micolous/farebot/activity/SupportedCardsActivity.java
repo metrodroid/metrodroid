61ef29f5c2a0f88938fb61c9d2787e4daaf18b52
Michael Farrell <micolous+git@gmail.com>
2015-08-16 01:09:05 +1000

    Detect Mifare Classic support and report which cards will not be supported in absence of device support (fixes #4), reorder supported cards alphabetically

--- src/main/java/au/id/micolous/farebot/activity/SupportedCardsActivity.java
+++ src/main/java/au/id/micolous/farebot/activity/SupportedCardsActivity.java
@@ -36,10 +36,15 @@ import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import au.id.micolous.farebot.FareBotApplication;
 import au.id.micolous.farebot.R;
+import au.id.micolous.farebot.card.CardType;
 import au.id.micolous.farebot.transit.manly_fast_ferry.ManlyFastFerryTransitData;
+import au.id.micolous.farebot.transit.opal.OpalTransitData;
+import au.id.micolous.farebot.util.Utils;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class SupportedCardsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
@@ -64,19 +69,75 @@ public class SupportedCardsActivity extends Activity {
     private class CardsAdapter extends ArrayAdapter<CardInfo> {
         public CardsAdapter(Context context) {
             super(context, 0, new ArrayList<CardInfo>());
-            add(new CardInfo(R.drawable.orca_card, "ORCA", R.string.location_seattle));
-            add(new CardInfo(R.drawable.clipper_card,          "Clipper",       R.string.location_san_francisco));
-            add(new CardInfo(R.drawable.ezlink_card,           "EZ-Link",       R.string.location_singapore,       R.string.card_note_ezlink));
-            add(new CardInfo(R.drawable.nets_card,             "NETS FlashPay", R.string.location_singapore));
-            add(new CardInfo(R.drawable.suica_card,            "Suica",         R.string.location_tokyo));
-            add(new CardInfo(R.drawable.pasmo_card,            "PASMO",         R.string.location_tokyo));
-            add(new CardInfo(R.drawable.edy_card,              "Edy",           R.string.location_tokyo));
-            add(new CardInfo(R.drawable.icoca_card,            "ICOCA",         R.string.location_kansai));
-            add(new CardInfo(R.drawable.ovchip_card,           "OV-chipkaart",  R.string.location_the_netherlands, R.string.card_note_ovchip));
-            add(new CardInfo(R.drawable.bilheteunicosp_card,   "Bilhete Único", R.string.location_sao_paulo,       R.string.card_note_bilheteunicosp));
-            add(new CardInfo(R.drawable.hsl_card,              "HSL",           R.string.location_helsinki_finland));
-            add(new CardInfo(R.drawable.opal_card,             "Opal",          R.string.location_sydney_australia));
-            add(new CardInfo(R.drawable.manly_fast_ferry_card, ManlyFastFerryTransitData.NAME, R.string.location_sydney_australia, R.string.card_note_manly_fast_ferry));
+
+            add(new CardInfo(R.drawable.bilheteunicosp_card, "Bilhete Único",
+                    R.string.location_sao_paulo,
+                    CardType.MifareClassic,
+                    true
+            ));
+
+            add(new CardInfo(R.drawable.clipper_card, "Clipper",
+                    R.string.location_san_francisco,
+                    CardType.MifareDesfire
+            ));
+
+            add(new CardInfo(R.drawable.edy_card, "Edy",
+                    R.string.location_tokyo,
+                    CardType.FeliCa
+            ));
+
+            add(new CardInfo(R.drawable.ezlink_card, "EZ-Link",
+                    R.string.location_singapore,
+                    CardType.CEPAS
+            ));
+
+            add(new CardInfo(R.drawable.hsl_card, "HSL",
+                    R.string.location_helsinki_finland,
+                    CardType.MifareDesfire
+            ));
+
+            add(new CardInfo(R.drawable.icoca_card, "ICOCA",
+                    R.string.location_kansai,
+                    CardType.FeliCa
+            ));
+
+            add(new CardInfo(R.drawable.manly_fast_ferry_card, ManlyFastFerryTransitData.NAME,
+                    R.string.location_sydney_australia,
+                    CardType.MifareClassic,
+                    true
+            ));
+
+            add(new CardInfo(R.drawable.nets_card, "NETS FlashPay",
+                    R.string.location_singapore,
+                    CardType.CEPAS
+            ));
+
+            add(new CardInfo(R.drawable.orca_card, "ORCA",
+                    R.string.location_seattle,
+                    CardType.MifareDesfire
+            ));
+
+            add(new CardInfo(R.drawable.opal_card, OpalTransitData.NAME,
+                    R.string.location_sydney_australia,
+                    CardType.MifareDesfire
+            ));
+
+            add(new CardInfo(R.drawable.ovchip_card, "OV-chipkaart",
+                    R.string.location_the_netherlands,
+                    CardType.MifareClassic,
+                    true
+            ));
+
+            add(new CardInfo(R.drawable.pasmo_card, "PASMO",
+                    R.string.location_tokyo,
+                    CardType.FeliCa
+            ));
+
+            add(new CardInfo(R.drawable.suica_card, "Suica",
+                    R.string.location_tokyo,
+                    CardType.FeliCa
+            ));
+
         }
         
         @Override public View getView(int position, View convertView, ViewGroup group) {
@@ -90,10 +151,29 @@ public class SupportedCardsActivity extends Activity {
             ((ImageView) convertView.findViewById(R.id.image)).setImageResource(info.getImageId());
             ((TextView)  convertView.findViewById(R.id.text)).setText(text);
 
-            if (info.getNoteId() >= 0) {
-                ((TextView) convertView.findViewById(R.id.note)).setText(info.getNoteId());
+            String notes = "";
+
+
+            if (info.getCardType() == CardType.MifareClassic && !FareBotApplication.getInstance().getMifareClassicSupport()) {
+                // Mifare Classic is not supported by this device.
+                notes += Utils.localizeString(R.string.card_not_supported_on_device) + " ";
             }
-            
+
+            if (info.getCardType() == CardType.CEPAS) {
+                // TODO: Implement feature detection for CEPAS like Mifare Classic.
+                // TODO: It is probably exposed in hasSystemFeature().
+                notes += Utils.localizeString(R.string.card_note_cepas) + " ";
+            }
+
+            // Keys being required is secondary to the card not being supported.
+            if (info.getKeysRequired()) {
+                notes += Utils.localizeString(R.string.keys_required) + " ";
+            }
+
+            if (!notes.equals("")) {
+                ((TextView) convertView.findViewById(R.id.note)).setText(notes);
+            }
+
             return convertView;
         }
     }
@@ -102,17 +182,23 @@ public class SupportedCardsActivity extends Activity {
         private final int mImageId;
         private final String mName;
         private final int mLocationId;
-        private final int mNoteId;
+        private final CardType mCardType;
+        private final boolean mKeysRequired;
 
         private CardInfo(int imageId, String name, int locationId) {
-            this(imageId, name, locationId, -1);
+            this(imageId, name, locationId, CardType.Unknown);
         }
 
-        private CardInfo(int imageId, String name, int locationId, int noteId) {
-            mImageId    = imageId;
-            mName       = name;
-            mLocationId = locationId;
-            mNoteId     = noteId;
+        private CardInfo(int imageId, String name, int locationId, CardType cardType) {
+            this(imageId, name, locationId, cardType, false);
+        }
+
+        private CardInfo(int imageId, String name, int locationId, CardType cardType, boolean keysRequired) {
+            mImageId      = imageId;
+            mName         = name;
+            mLocationId   = locationId;
+            mCardType     = cardType;
+            mKeysRequired = keysRequired;
         }
 
         public int getImageId() {
@@ -127,8 +213,12 @@ public class SupportedCardsActivity extends Activity {
             return mLocationId;
         }
 
-        public int getNoteId() {
-            return mNoteId;
+        public CardType getCardType() {
+            return mCardType;
+        }
+
+        public boolean getKeysRequired() {
+            return mKeysRequired;
         }
     }
 }
