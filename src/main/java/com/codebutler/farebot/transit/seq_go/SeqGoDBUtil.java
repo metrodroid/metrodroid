63102490078c919d5179028fa31fdcbb85ba44cf
Michael Farrell <micolous+git@gmail.com>
2016-03-30 20:41:57 +1000

    go card: Add South Brisbane station, fix `make clean`.

--- src/main/java/com/codebutler/farebot/transit/seq_go/SeqGoDBUtil.java
+++ src/main/java/com/codebutler/farebot/transit/seq_go/SeqGoDBUtil.java
@@ -44,7 +44,7 @@ public class SeqGoDBUtil extends DBUtil {
 
     private static final String DB_NAME = "seq_go_stations.db3";
 
-    private static final int VERSION = 3720;
+    private static final int VERSION = 3721;
 
     public SeqGoDBUtil(Context context) {
         super(context);
