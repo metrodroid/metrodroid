c840bb355a1b5077d009361c73230c4da4e09eb3
Michael Farrell <micolous+git@gmail.com>
2016-11-05 00:03:18 +1100

    mfcdump_to_farebotxml: support exporting many cards at once

--- extra/mfcdump_to_farebotxml.py
+++ extra/mfcdump_to_farebotxml.py
@@ -3,7 +3,7 @@
 mfcdump_to_farebotxml.py - Converts a mfoc/mfcuk .mfc dump file for Mifare
 Classic to Farebot XML export format.
 
-Copyright 2015 Michael Farrell <micolous+git@gmail.com>
+Copyright 2015-2016 Michael Farrell <micolous+git@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,40 +25,51 @@ from os.path import getmtime
 from xml.etree import ElementTree as etree
 
 
-def mfc_to_farebot(input_f, output_f):
-	# Read the Mifare card entirely first
-	card_data = input_f.read()
+def mfc_to_farebot(input_fs, output_f):
+	cards = []
 
-	# Card data should be 1K or 4K
-	assert len(card_data) in (1024, 4096)
+	for input_f in input_fs:
+		# Read the Mifare card entirely first
+		card_data = input_f.read()
 
-	# Lets make some XML.
-	root = etree.Element('card', type='0', id=b16encode(card_data[0:4]).lower(), scanned_at=str(int(getmtime(input_f.name)*1000)))
-	sectors = etree.SubElement(root, 'sectors')
+		# Card data should be 1K or 4K
+		assert len(card_data) in (1024, 4096)
 
-	if len(card_data) == 1024:
-		sector_count = 16
-	elif len(card_data) == 4096:
-		sector_count = 40
+		# Lets make some XML.
+		card = etree.Element('card', type='0', id=b16encode(card_data[0:4]).lower(), scanned_at=str(int(getmtime(input_f.name)*1000)))
+		sectors = etree.SubElement(card, 'sectors')
+
+		if len(card_data) == 1024:
+			sector_count = 16
+		elif len(card_data) == 4096:
+			sector_count = 40
 		
-	for sector_no in range(sector_count):
-		sector = etree.SubElement(sectors, 'sector', index=str(sector_no))
-		blocks = etree.SubElement(sector, 'blocks')
-		if sector_no < 32:
-			block_count = 4
-		else:
-			block_count = 16
-
-		for block_no in range(block_count):
-			block = etree.SubElement(blocks, 'block', index=str(block_no), type='data')
-			data = etree.SubElement(block, 'data')
-			
+		for sector_no in range(sector_count):
+			sector = etree.SubElement(sectors, 'sector', index=str(sector_no))
+			blocks = etree.SubElement(sector, 'blocks')
 			if sector_no < 32:
-				offset = (sector_no * 64) + (block_no * 16)
+				block_count = 4
 			else:
-				offset = 2048 + ((sector_no - 32) * 256) + (block_no * 16)
+				block_count = 16
+
+			for block_no in range(block_count):
+				block = etree.SubElement(blocks, 'block', index=str(block_no), type='data')
+				data = etree.SubElement(block, 'data')
 			
-			data.text = b64encode(card_data[offset:offset+16])
+				if sector_no < 32:
+					offset = (sector_no * 64) + (block_no * 16)
+				else:
+					offset = 2048 + ((sector_no - 32) * 256) + (block_no * 16)
+
+				data.text = b64encode(card_data[offset:offset+16])
+
+		cards.append(card)
+
+	if len(cards) > 1:
+		root = etree.Element('cards')
+		root.extend(cards)
+	else:
+		root = cards[0]
 
 	# We have made a structure, dump it to disk
 	tree = etree.ElementTree(root)
@@ -70,14 +81,14 @@ def mfc_to_farebot(input_f, output_f):
 
 def main():
 	parser = ArgumentParser()
-	parser.add_argument('input_mfc', nargs=1, type=FileType('rb'),
-		help='Card MFC dump to read')
+	parser.add_argument('input_mfc', nargs='+', type=FileType('rb'),
+		help='Card MFC dumps to read')
 
 	parser.add_argument('-o', '--output', type=FileType('wb'),
 		help='Output Farebot XML to write')
 
 	options = parser.parse_args()
-	mfc_to_farebot(options.input_mfc[0], options.output)
+	mfc_to_farebot(options.input_mfc, options.output)
 
 
 if __name__ == '__main__':
