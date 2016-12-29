6b0de5ef4cda0da89a56bccf54375d75e1fbff3e
Michael Farrell <micolous+git@gmail.com>
2016-12-29 19:21:42 +1000

    seq_go: more stations

--- src/main/java/com/codebutler/farebot/transit/seq_go/SeqGoDBUtil.java
+++ src/main/java/com/codebutler/farebot/transit/seq_go/SeqGoDBUtil.java
@@ -54,7 +54,7 @@ public class SeqGoDBUtil extends DBUtil {
 
     private static final String DB_NAME = "seq_go_stations.db3";
 
-    private static final int VERSION = 3951;
+    private static final int VERSION = 4010;
 
     public SeqGoDBUtil(Context context) {
         super(context);
