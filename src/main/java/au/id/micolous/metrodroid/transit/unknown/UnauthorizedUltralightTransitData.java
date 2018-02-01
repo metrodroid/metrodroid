d95ec5ee44a05f830576ae4ebc07111feb74918e
Michael Farrell <micolous+git@gmail.com>
2018-02-01 21:04:16 +1100

    fix comments

--- src/main/java/au/id/micolous/metrodroid/transit/unknown/UnauthorizedUltralightTransitData.java
+++ src/main/java/au/id/micolous/metrodroid/transit/unknown/UnauthorizedUltralightTransitData.java
@@ -27,7 +27,7 @@ import au.id.micolous.metrodroid.transit.TransitIdentity;
 import au.id.micolous.metrodroid.util.Utils;
 
 /**
- * Handle MiFare Classic with no open sectors
+ * Handle MiFare Ultralight with no open sectors
  */
 public class UnauthorizedUltralightTransitData extends UnauthorizedTransitData {
     /**
