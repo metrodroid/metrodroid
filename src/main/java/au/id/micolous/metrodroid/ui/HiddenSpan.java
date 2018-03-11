028e2534f543432ba1c6831702c2e35de2bb7177
Michael Farrell <micolous+git@gmail.com>
2018-03-11 18:24:09 +1100

    Explain HiddenSpan

--- src/main/java/au/id/micolous/metrodroid/ui/HiddenSpan.java
+++ src/main/java/au/id/micolous/metrodroid/ui/HiddenSpan.java
@@ -1,3 +1,21 @@
+/*
+ * HiddenSpan.java
+ *
+ * Copyright 2018 Michael Farrell <micolous+git@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package au.id.micolous.metrodroid.ui;
 
 import android.graphics.Canvas;
@@ -8,6 +26,12 @@ import android.text.style.ReplacementSpan;
 
 /**
  * Span which is not drawn on screen.
+ *
+ * This is used to include elements in a TextView which a screen reader should speak, but
+ * should not be displayed on the screen.
+ *
+ * This is used to make some labels sound more natural, where they otherwise require
+ * Unicode characters without sufficient meaning to be read.
  */
 
 public class HiddenSpan extends ReplacementSpan {
