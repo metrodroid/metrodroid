41f0e6fadce55eedd47c838ed7ed1c57b2c20677
Michael Farrell <micolous+git@gmail.com>
2018-08-15 22:19:41 +1000

    Show unauthorised card warning for fully locked DESFire cards

--- src/main/java/au/id/micolous/metrodroid/activity/CardInfoActivity.java
+++ src/main/java/au/id/micolous/metrodroid/activity/CardInfoActivity.java
@@ -21,7 +21,6 @@
 package au.id.micolous.metrodroid.activity;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -38,6 +37,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import au.id.micolous.farebot.R;
+import au.id.micolous.metrodroid.MetrodroidApplication;
 import au.id.micolous.metrodroid.card.Card;
 import au.id.micolous.metrodroid.card.UnsupportedCardException;
 import au.id.micolous.metrodroid.fragment.BlankCardFragment;
@@ -49,13 +50,9 @@ import au.id.micolous.metrodroid.provider.CardsTableColumns;
 import au.id.micolous.metrodroid.transit.TransitBalance;
 import au.id.micolous.metrodroid.transit.TransitData;
 import au.id.micolous.metrodroid.transit.unknown.BlankUltralightTransitData;
-import au.id.micolous.metrodroid.transit.unknown.UnauthorizedClassicTransitData;
-import au.id.micolous.metrodroid.transit.unknown.UnauthorizedUltralightTransitData;
+import au.id.micolous.metrodroid.transit.unknown.UnauthorizedTransitData;
 import au.id.micolous.metrodroid.ui.TabPagerAdapter;
 import au.id.micolous.metrodroid.util.Utils;
-
-import au.id.micolous.farebot.R;
-import au.id.micolous.metrodroid.MetrodroidApplication;
 
 /**
  * @author Eric Butler
@@ -159,8 +156,7 @@ public class CardInfoActivity extends MetrodroidActivity {
                         mCard.toXml(MetrodroidApplication.getInstance().getSerializer()));
                 args.putParcelable(EXTRA_TRANSIT_DATA, mTransitData);
 
-                if (mTransitData instanceof UnauthorizedClassicTransitData ||
-                        mTransitData instanceof UnauthorizedUltralightTransitData) {
+                if (mTransitData instanceof UnauthorizedTransitData) {
                     mTabsAdapter.addTab(actionBar.newTab(), UnauthorizedCardFragment.class, args);
                     return;
                 }
