4caadd4621813bf0214149e07019e52bee44389a
Vladimir Serbinenko <phcoder@google.com>
2018-08-26 23:25:38 +0000

    Fix MIME-types for keys.
    We don't support XML keys. Add x-extesion-bin to accepted types

--- src/main/java/au/id/micolous/metrodroid/fragment/KeysFragment.java
+++ src/main/java/au/id/micolous/metrodroid/fragment/KeysFragment.java
@@ -161,7 +161,7 @@ public class KeysFragment extends ListFragment implements AdapterView.OnItemLong
             // In Android 4.4 and later, we can say the right thing!
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 i.setType("*/*");
-                String[] mimetypes = {"application/xml", "text/xml", "application/octet-stream"};
+                String[] mimetypes = {"application/octet-stream", "application/x-extension-bin" };
                 i.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
             } else {
                 // Failsafe, used in the emulator for local files
