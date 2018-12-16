33beeb0f51223b8f4dd3380bb402411ae6d170e4
Vladimir Serbinenko <phcoder@google.com>
2018-12-16 21:16:22 +0000

    Dump SFI on Calypso
    Pisa Carta Mobile implements Calypso interface but works only with
    SFI selecftions, hence dump SFI.

--- src/main/java/au/id/micolous/metrodroid/card/calypso/CalypsoApplication.java
+++ src/main/java/au/id/micolous/metrodroid/card/calypso/CalypsoApplication.java
@@ -102,12 +102,19 @@ public class CalypsoApplication extends ISO7816Application {
         int counter = 0;
         boolean partialRead = false;
 
+        for (byte sfi = 1; sfi <= 31; sfi++) {
+            feedbackInterface.updateProgressBar(counter++, File.getAll().length + 31);
+            ISO7816File sfiFile = appData.dumpFileSFI(protocol, sfi, 0);
+            if (sfiFile != null && sfi == File.TICKETING_ENVIRONMENT.mSfi)
+                showCardType(sfiFile, feedbackInterface);
+        }
+
         for (File f : File.getAll()) {
-            feedbackInterface.updateProgressBar(counter++, File.getAll().length);
+            feedbackInterface.updateProgressBar(counter++, File.getAll().length + 31);
             try {
-                appData.dumpFile(protocol, f.getSelector(), 0x1d);
-                if (f == File.TICKETING_ENVIRONMENT)
-                    showCardType(appData, feedbackInterface);
+                ISO7816File file = appData.dumpFile(protocol, f.getSelector(), 0x1d);
+                if (file != null && f == File.TICKETING_ENVIRONMENT)
+                    showCardType(file, feedbackInterface);
             } catch (TagLostException e) {
                 Log.w(TAG, "tag lost", e);
                 partialRead = true;
@@ -120,12 +127,9 @@ public class CalypsoApplication extends ISO7816Application {
         return new CalypsoApplication(appData, partialRead);
     }
 
-    private static void showCardType(ISO7816Application.ISO7816Info appData, TagReaderFeedbackInterface feedbackInterface) {
+    private static void showCardType(ISO7816File tenvf, TagReaderFeedbackInterface feedbackInterface) {
         byte[] tenv;
         try {
-            ISO7816File tenvf = appData.getFile(File.TICKETING_ENVIRONMENT.getSelector());
-            if (tenvf == null)
-                return;
             tenv = tenvf.getRecord(1).getData();
         } catch (Exception e) {
             return;
@@ -185,8 +189,18 @@ public class CalypsoApplication extends ISO7816Application {
         return null;
     }
 
+    public ISO7816File getFile(File f, boolean trySfi) {
+        ISO7816File ff = getFile(f.getSelector());
+        if (ff != null)
+            return ff;
+        int invId = f.mSfi;
+        if (!trySfi || invId < 0 || invId >= 0x20)
+            return null;
+        return getSfiFile(invId);
+    }
+
     public ISO7816File getFile(File f) {
-        return getFile(f.getSelector());
+        return getFile(f, true);
     }
 
     @Override
@@ -257,87 +271,113 @@ public class CalypsoApplication extends ISO7816Application {
         return null;
     }
 
-    public enum File {
-        // Put this first to be able to show card image as soon as possible
-        TICKETING_ENVIRONMENT(0x2000, 0x2001),
-
-        AID(0x3F04),
-        ICC(0x0002),
-        ID(0x0003),
-        HOLDER_EXTENDED(0x3F1C),
-        DISPLAY(0x2F10),
-
-        TICKETING_HOLDER(0x2000, 0x2002),
-        TICKETING_AID(0x2000, 0x2004),
-        TICKETING_LOG(0x2000, 0x2010),
-        TICKETING_CONTRACTS_1(0x2000, 0x2020),
-        TICKETING_CONTRACTS_2(0x2000, 0x2030),
-        TICKETING_COUNTERS_1(0x2000, 0x202A),
-        TICKETING_COUNTERS_2(0x2000, 0x202B),
-        TICKETING_COUNTERS_3(0x2000, 0x202C),
-        TICKETING_COUNTERS_4(0x2000, 0x202D),
-        TICKETING_COUNTERS_5(0x2000, 0x202E),
-        TICKETING_COUNTERS_6(0x2000, 0x202F),
-        TICKETING_SPECIAL_EVENTS(0x2000, 0x2040),
-        TICKETING_CONTRACT_LIST(0x2000, 0x2050),
-        TICKETING_COUNTERS_7(0x2000, 0x2060),
-        TICKETING_COUNTERS_8(0x2000, 0x2062),
-        TICKETING_COUNTERS_9(0x2000, 0x2069),
-        TICKETING_COUNTERS_10(0x2000, 0x206A),
-        TICKETING_FREE(0x2000, 0x20F0),
-
-        // Parking application (MPP)
-        MPP_PUBLIC_PARAMETERS(0x3100, 0x3102),
-        MPP_AID(0x3100, 0x3104),
-        MPP_LOG(0x3100, 0x3115),
-        MPP_CONTRACTS(0x3100, 0x3120),
-        MPP_COUNTERS_1(0x3100, 0x3113),
-        MPP_COUNTERS_2(0x3100, 0x3123),
-        MPP_COUNTERS_3(0x3100, 0x3133),
-        MPP_MISCELLANEOUS(0x3100, 0x3150),
-        MPP_COUNTERS_4(0x3100, 0x3169),
-        MPP_FREE(0x3100, 0x31F0),
-
-        // Transport application (RT)
-        RT2_ENVIRONMENT(0x2100, 0x2101),
-        RT2_AID(0x2100, 0x2104),
-        RT2_LOG(0x2100, 0x2110),
-        RT2_CONTRACTS(0x2100, 0x2120),
-        RT2_SPECIAL_EVENTS(0x2100, 0x2140),
-        RT2_CONTRACT_LIST(0x2100, 0x2150),
-        RT2_COUNTERS(0x2100, 0x2169),
-        RT2_FREE(0x2100, 0x21F0),
-
-        EP_AID(0x1000, 0x1004),
-        EP_LOAD_LOG(0x1000, 0x1014),
-        EP_PURCHASE_LOG(0x1000, 0x1015),
-
-        ETICKET(0x8000, 0x8004),
-        ETICKET_EVENT_LOGS(0x8000, 0x8010),
-        ETICKET_PRESELECTION(0x8000, 0x8030);
-
-        private ISO7816Selector mSelector;
-
-        File(int file) {
-            mSelector = ISO7816Selector.makeSelector(file);
-        }
-
-        File(int folder, int file) {
-            mSelector = ISO7816Selector.makeSelector(folder, file);
-        }
-
-        public static File[] getAll() {
-            return File.class.getEnumConstants();
-        }
-
-        public ISO7816Selector getSelector() {
-            return mSelector;
-        }
+    @Override
+    protected String nameSfiFile(int sfi) {
+        if (SFI_MAP[sfi] != null)
+            return SFI_MAP[sfi].name();
+        return null;
     }
+
+     @SuppressWarnings("unused")
+     public enum File {
+         // Put this first to be able to show card image as soon as possible
+        TICKETING_ENVIRONMENT(0x07, 0x2000, 0x2001), // SFI from spec
+
+         AID(0x04, ISO7816Selector.makeSelector(0x3F04)), // SFI empirical
+         ICC(0x02, ISO7816Selector.makeSelector(0x0002)), // SFI empirical
+         ID(0x03, ISO7816Selector.makeSelector(0x0003)), // SFI empirical
+         HOLDER_EXTENDED(0x3F1C),
+         DISPLAY(0x05, ISO7816Selector.makeSelector(0x2F10)), // SFI empirical
+
+         TICKETING_HOLDER(0x2000, 0x2002),
+         TICKETING_AID(0x2000, 0x2004),
+         TICKETING_LOG(0x08, 0x2000, 0x2010), // SFI from spec
+         TICKETING_CONTRACTS_1(0x09, 0x2000, 0x2020), // SFI from spec
+         TICKETING_CONTRACTS_2(0x06, 0x2000, 0x2030), // SFI empirical
+         TICKETING_COUNTERS_1(0x0a, 0x2000, 0x202A), // SFI empirical
+         TICKETING_COUNTERS_2(0x0b, 0x2000, 0x202B), // SFI empirical
+         TICKETING_COUNTERS_3(0x0c, 0x2000, 0x202C), // SFI empirical
+         TICKETING_COUNTERS_4(0x0d, 0x2000, 0x202D), // SFI empirical
+         TICKETING_COUNTERS_5(0x2000, 0x202E),
+         TICKETING_COUNTERS_6(0x2000, 0x202F),
+         TICKETING_SPECIAL_EVENTS(0x1d, 0x2000, 0x2040), // SFI from spec
+         TICKETING_CONTRACT_LIST(0x1e, 0x2000, 0x2050), // SFI from spec
+         TICKETING_COUNTERS_7(0x2000, 0x2060),
+         TICKETING_COUNTERS_8(0x2000, 0x2062),
+         TICKETING_COUNTERS_9(0x19, 0x2000, 0x2069),  // SFI from spec
+         TICKETING_COUNTERS_10(0x10, 0x2000, 0x206A), // SFI empirical
+         TICKETING_FREE(0x01, 0x2000, 0x20F0),
+
+         // Parking application (MPP)
+         MPP_PUBLIC_PARAMETERS(0x17, 0x3100, 0x3102), // SFI empirical
+         MPP_AID(0x3100, 0x3104),
+         MPP_LOG(0x3100, 0x3115),
+         MPP_CONTRACTS(0x3100, 0x3120),
+         MPP_COUNTERS_1(0x3100, 0x3113),
+         MPP_COUNTERS_2(0x3100, 0x3123),
+         MPP_COUNTERS_3(0x3100, 0x3133),
+         MPP_MISCELLANEOUS(0x3100, 0x3150),
+         MPP_COUNTERS_4(0x3100, 0x3169),
+         MPP_FREE(0x3100, 0x31F0),
+
+         // Transport application (RT)
+         RT2_ENVIRONMENT(0x2100, 0x2101),
+         RT2_AID(0x2100, 0x2104),
+         RT2_LOG(0x2100, 0x2110),
+         RT2_CONTRACTS(0x2100, 0x2120),
+         RT2_SPECIAL_EVENTS(0x2100, 0x2140),
+         RT2_CONTRACT_LIST(0x2100, 0x2150),
+         RT2_COUNTERS(0x2100, 0x2169),
+         RT2_FREE(0x2100, 0x21F0),
+
+         EP_AID(0x1000, 0x1004),
+         EP_LOAD_LOG(0x14, 0x1000, 0x1014), // SFI empirical
+         EP_PURCHASE_LOG(0x15, 0x1000, 0x1015), // SFI empirical
+
+         ETICKET(0x8000, 0x8004),
+         ETICKET_EVENT_LOGS(0x8000, 0x8010),
+         ETICKET_PRESELECTION(0x8000, 0x8030);
+
+         private ISO7816Selector mSelector;
+         private final int mSfi;
+
+         File(int file) {
+             this(-1, ISO7816Selector.makeSelector(file));
+         }
+
+         File(int folder, int file) {
+             this(-1, ISO7816Selector.makeSelector(folder, file));
+         }
+
+         File(int sfi, int folder, int file) {
+             this(sfi, ISO7816Selector.makeSelector(folder, file));
+         }
+
+         File(int sfi, ISO7816Selector selector) {
+             mSelector = selector;
+             mSfi = sfi;
+         }
+
+         File(ISO7816Selector selector) {
+             this(-1, selector);
+         }
+
+         public static File[] getAll() {
+             return File.class.getEnumConstants();
+         }
+
+         public ISO7816Selector getSelector() {
+             return mSelector;
+         }
+     }
+
+    private static final File[] SFI_MAP = new File[0x20];
 
     static {
         for (File f : File.getAll()) {
             NAME_MAP.put(f.mSelector.formatString(), f.name());
+            if (f.mSfi >= 0 && f.mSfi < SFI_MAP.length)
+                SFI_MAP[f.mSfi] = f;
         }
     }
 }
