5bd577b6cdf93a2ba68a852e2cc94b28982a6856
Vladimir Serbinenko <phcoder@google.com>
2018-12-25 16:20:48 +0000

    Remove unused imports in NextfareUnknownUltralightTransitData and ClipperRefill

--- src/main/java/au/id/micolous/metrodroid/transit/clipper/ClipperRefill.java
+++ src/main/java/au/id/micolous/metrodroid/transit/clipper/ClipperRefill.java
@@ -25,7 +25,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import java.lang.Comparable;
 import java.util.Calendar;
 
 import au.id.micolous.metrodroid.transit.TransitCurrency;
