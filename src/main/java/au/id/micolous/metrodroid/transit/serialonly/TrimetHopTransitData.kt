36dbed9147e3d36c86e68706e94b9431f829231b
Vladimir Serbinenko <phcoder@google.com>
2019-01-24 17:38:11 +0000

    Desfire TrimetHop NPE-safety

--- src/main/java/au/id/micolous/metrodroid/transit/serialonly/TrimetHopTransitData.kt
+++ src/main/java/au/id/micolous/metrodroid/transit/serialonly/TrimetHopTransitData.kt
@@ -85,8 +85,8 @@ data class TrimetHopTransitData(private val mSerial: Int?,
                     mIssueDate = issueDate)
         }
 
-        private fun parseSerial(app: DesfireApplication) =
-                app.getFile(0)?.data?.byteArrayToInt(0xc, 4)
+        private fun parseSerial(app: DesfireApplication?) =
+                app?.getFile(0)?.data?.byteArrayToInt(0xc, 4)
 
         val FACTORY: DesfireCardTransitFactory = object : DesfireCardTransitFactory {
             override fun earlyCheck(appIds: IntArray) = APP_ID in appIds
