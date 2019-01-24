f1f779c7b6ce001bbcdf9ec07410272ea189bea5
Vladimir Serbinenko <phcoder@google.com>
2019-01-24 21:20:00 +0000

    Convert UltralightCardTransitFactory to kotlin

--- src/main/java/au/id/micolous/metrodroid/card/ultralight/UltralightCardTransitFactory.java
+++
@@ -1,5 +0,0 @@
-package au.id.micolous.metrodroid.card.ultralight;
-
-import au.id.micolous.metrodroid.transit.CardTransitFactory;
-
-public interface UltralightCardTransitFactory extends CardTransitFactory<UltralightCard> {}
