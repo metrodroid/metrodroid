55eb852d77b368d6ee27b573476d50ab6a0bf96a
Vladimir Serbinenko <phcoder@google.com>
2019-01-25 21:27:00 +0000

    Switch podorozhnik to read sector

--- src/main/java/au/id/micolous/metrodroid/transit/podorozhnik/PodorozhnikTransitData.java
+++ src/main/java/au/id/micolous/metrodroid/transit/podorozhnik/PodorozhnikTransitData.java
@@ -156,9 +156,9 @@ public class PodorozhnikTransitData extends TransitData {
         mCountersValid = p.readInt() != 0;
     }
 
-    private static String getSerial(ImmutableByteArray uid) {
+    private static String getSerial(ImmutableByteArray sector0) {
         String sn;
-        sn = "9643 3078 " + NumberUtils.INSTANCE.formatNumber(uid.byteArrayToLongReversed(0, 7),
+        sn = "9643 3078 " + NumberUtils.INSTANCE.formatNumber(sector0.byteArrayToLongReversed(0, 7),
                 " ", 4, 4, 4, 4, 1);
         sn += NumberUtils.INSTANCE.calculateLuhn (sn.replaceAll(" ", ""));// last digit is luhn
         return sn;
@@ -215,7 +215,7 @@ public class PodorozhnikTransitData extends TransitData {
     }
 
     public PodorozhnikTransitData(ClassicCard card) {
-        mSerial = getSerial(card.getTagId());
+        mSerial = getSerial(card.getSector(0).getBlock(0).getData());
         mExtraTripTimes = new ArrayList<>();
         decodeSector4(card);
         decodeSector5(card);
@@ -276,7 +276,7 @@ public class PodorozhnikTransitData extends TransitData {
         @Override
         public TransitIdentity parseTransitIdentity(@NonNull ClassicCard card) {
             return new TransitIdentity(Localizer.INSTANCE.localizeString(R.string.card_name_podorozhnik),
-                    getSerial(card.getTagId()));
+                    getSerial(card.getSector(0).getBlock(0).getData()));
         }
 
         @Override
