729b369cb00407f58456a0c3b3ab14063f5dae05
Vladimir Serbinenko <phcoder@google.com>
2019-01-28 01:09:13 +0000

    Uplift KMT to kotlin-common

---
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/kmt/KMTTransitData.kt
@@ -0,0 +1,118 @@
+/*
+ * KMTTransitData.java
+ *
+ * Copyright 2018 Bondan Sumbodo <sybond@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package au.id.micolous.metrodroid.transit.kmt
+
+import au.id.micolous.metrodroid.time.Epoch
+import au.id.micolous.metrodroid.time.MetroTimeZone
+import au.id.micolous.metrodroid.transit.*
+import au.id.micolous.metrodroid.card.CardType
+import au.id.micolous.metrodroid.card.felica.FelicaCard
+import au.id.micolous.metrodroid.card.felica.FelicaCardTransitFactory
+import au.id.micolous.metrodroid.multi.Parcelize
+import au.id.micolous.metrodroid.util.Preferences
+import au.id.micolous.metrodroid.multi.R
+import au.id.micolous.metrodroid.ui.HeaderListItem
+import au.id.micolous.metrodroid.ui.ListItem
+
+@Parcelize
+class KMTTransitData (override val trips: List<KMTTrip>,
+                      override val serialNumber: String?,
+                      private val mCurrentBalance: Int,
+                      private val mTransactionCounter: Int,
+                      private val mLastTransAmount: Int): TransitData() {
+
+    public override val balance: TransitCurrency?
+        get() = TransitCurrency.IDR(mCurrentBalance)
+
+    override val cardName: String
+        get() = NAME
+
+    override val info: List<ListItem>?
+        get() = listOfNotNull(
+                HeaderListItem(R.string.kmt_other_data),
+                if (!Preferences.hideCardNumbers) {
+                    ListItem(R.string.transaction_counter, mTransactionCounter.toString())
+                } else null,
+                ListItem(R.string.kmt_last_trx_amount,
+                        TransitCurrency.IDR(mLastTransAmount).maybeObfuscateFare().formatCurrencyString(false)))
+
+    companion object {
+        // defines
+        private const val NAME = "Kartu Multi Trip"
+        private const val SYSTEMCODE_KMT = 0x90b7
+        private const val FELICA_SERVICE_KMT_ID = 0x300B
+        private const val FELICA_SERVICE_KMT_BALANCE = 0x1017
+        private const val FELICA_SERVICE_KMT_HISTORY = 0x200F
+        val KMT_EPOCH = Epoch.utc(2000, MetroTimeZone.JAKARTA, -6 * 60)
+
+        private fun parse(card: FelicaCard): KMTTransitData {
+            val serialNumber = getSerial(card)
+            val serviceBalance = card.getSystem(SYSTEMCODE_KMT)?.getService(FELICA_SERVICE_KMT_BALANCE)
+            val blocksBalance = serviceBalance?.blocks
+            val blockBalance = blocksBalance?.get(0)
+            val dataBalance = blockBalance?.data
+            val mCurrentBalance = dataBalance?.byteArrayToIntReversed(0, 4) ?: 0
+            val mTransactionCounter = dataBalance?.byteArrayToInt(13, 3) ?: 0
+            val mLastTransAmount = dataBalance?.byteArrayToIntReversed(4, 4) ?: 0
+
+            val trips = card.getSystem(SYSTEMCODE_KMT)
+                    ?.getService(FELICA_SERVICE_KMT_HISTORY)?.blocks
+                    ?.mapNotNull { block ->
+                        if (block.data[0].toInt() != 0 && block.data.byteArrayToInt(8, 2) != 0) {
+                            KMTTrip.parse(block)
+                        } else
+                            null
+                    }
+            return KMTTransitData(trips = trips.orEmpty(), serialNumber = serialNumber, mCurrentBalance = mCurrentBalance,
+                    mTransactionCounter = mTransactionCounter, mLastTransAmount = mLastTransAmount)
+        }
+
+        private val CARD_INFO = CardInfo(
+                imageId = R.drawable.kmt_card,
+                name = KMTTransitData.NAME,
+                locationId = R.string.location_jakarta,
+                cardType = CardType.FeliCa,
+                resourceExtraNote = R.string.kmt_extra_note)
+
+        private fun getSerial(card: FelicaCard): String? {
+            val dataID = card.getSystem(SYSTEMCODE_KMT)?.getService(FELICA_SERVICE_KMT_ID)
+                    ?.blocks?.get(0)?.data ?: return null
+            return if (dataID.isASCII())
+                dataID.readASCII()
+            else
+                dataID.toHexString()
+        }
+
+        val FACTORY: FelicaCardTransitFactory = object : FelicaCardTransitFactory () {
+
+            override val allCards: List<CardInfo>
+                get() = listOf(CARD_INFO)
+
+            override fun earlyCheck(systemCodes: List<Int>) = SYSTEMCODE_KMT in systemCodes
+
+            override fun parseTransitData(card: FelicaCard) = parse(card)
+
+            override fun parseTransitIdentity(card: FelicaCard) = TransitIdentity(NAME,
+                    card.getSystem(SYSTEMCODE_KMT)?.getService(FELICA_SERVICE_KMT_ID)?.
+                            getBlock(0)?.data?.readASCII() ?: "-")
+        }
+    }
+}
+
---
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/kmt/KMTTrip.kt
@@ -0,0 +1,90 @@
+/*
+ * KMTTrip.java
+ *
+ * Copyright 2018 Bondan Sumbodo <sybond@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package au.id.micolous.metrodroid.transit.kmt
+
+import au.id.micolous.metrodroid.card.felica.FelicaBlock
+import au.id.micolous.metrodroid.multi.Localizer
+import au.id.micolous.metrodroid.multi.Parcelize
+import au.id.micolous.metrodroid.multi.R
+import au.id.micolous.metrodroid.time.Timestamp
+import au.id.micolous.metrodroid.transit.Station
+import au.id.micolous.metrodroid.transit.TransitCurrency
+import au.id.micolous.metrodroid.transit.Trip
+import au.id.micolous.metrodroid.util.StationTableReader
+import au.id.micolous.metrodroid.util.ImmutableByteArray
+
+@Parcelize
+class KMTTrip (private val mProcessType: Int,
+               private val mSequenceNumber: Int,
+               override val startTimestamp: Timestamp?,
+               private val mTransactionAmount: Int,
+               private val mEndGateCode: Int): Trip() {
+
+    // Normally, only the end station is recorded.  But top-ups only have a "starting" station.
+    override val startStation: Station?
+        get() = if (mProcessType == 0 || mProcessType == 2) {
+            getStation(mEndGateCode)
+        } else null
+
+    // "Ending station" doesn't make sense for Ticket Machines or Point-of-sale
+    override val endStation: Station?
+        get() = if (mProcessType == 0 || mProcessType == 2) {
+            null
+        } else getStation(mEndGateCode)
+
+    override val mode: Trip.Mode
+        get() = when (mProcessType) {
+            0 -> Trip.Mode.TICKET_MACHINE
+            1 -> Trip.Mode.TRAIN
+            2 -> Trip.Mode.POS
+            else -> Trip.Mode.OTHER
+        }
+
+    override val fare: TransitCurrency?
+        get() = if (mProcessType != 1) {
+            TransitCurrency.IDR(mTransactionAmount).negate()
+        } else TransitCurrency.IDR(mTransactionAmount)
+
+    override fun getAgencyName(isShort: Boolean) = Localizer.localizeString(R.string.kmt_agency)
+
+    companion object {
+        fun parse(block: FelicaBlock): KMTTrip {
+            val data = block.data
+            return KMTTrip(
+                    mProcessType = data[12].toInt() and 0xff,
+                    mSequenceNumber = data.byteArrayToInt(13, 3),
+                    startTimestamp = calcDate(data),
+                    mTransactionAmount = data.byteArrayToInt(4, 4),
+                    mEndGateCode = data.byteArrayToInt(8, 2))
+        }
+
+        private const val KMT_STR = "kmt"
+
+        private fun calcDate(data: ImmutableByteArray): Timestamp? {
+            val fulloffset = data.byteArrayToInt(0, 4)
+            if (fulloffset == 0) {
+                return null
+            }
+            return KMTTransitData.KMT_EPOCH.seconds(fulloffset.toLong())
+        }
+
+        private fun getStation(code: Int) = StationTableReader.getStation(KMT_STR, code)
+    }
+}
