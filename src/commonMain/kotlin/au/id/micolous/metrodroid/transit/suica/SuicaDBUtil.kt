f3eb24068aeb6eca6182e411b3854ca154692f43
Vladimir Serbinenko <phcoder@google.com>
2019-01-28 01:15:57 +0000

    Uplift Suica to kotlin-common

---
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/suica/SuicaDBUtil.kt
@@ -0,0 +1,83 @@
+/*
+ * SuicaDBUtil.java
+ *
+ * Copyright 2018 Michael Farrell <micolous+git@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package au.id.micolous.metrodroid.transit.suica
+
+import au.id.micolous.metrodroid.multi.Localizer
+import au.id.micolous.metrodroid.multi.R
+import au.id.micolous.metrodroid.transit.Station
+import au.id.micolous.metrodroid.util.NumberUtils
+import au.id.micolous.metrodroid.util.StationTableReader
+
+/**
+ * Helpers for accessing Suica-related stop databases.
+ */
+
+object SuicaDBUtil {
+    private const val TAG = "SuicaUtil"
+    private const val SUICA_BUS_STR = "suica_bus"
+    private const val SUICA_RAIL_STR = "suica_rail"
+
+    /**
+     * Gets bus stop information from the IruCa (イルカ) table.
+     *
+     * @param lineCode    Bus line ID (line code)
+     * @param stationCode Bus stop ID (station code)
+     * @return If the stop is known, a Station is returned describing it. If the stop is unknown,
+     * or there was some other database error, null is returned.
+     */
+    internal fun getBusStop(regionCode: Int, lineCode: Int, stationCode: Int): Station? {
+        var lineCode = lineCode
+        var stationCode = stationCode
+        lineCode = lineCode and 0xFF
+        stationCode = stationCode and 0xFF
+
+        val stationId = (lineCode shl 8) + stationCode
+        return if (stationId == 0) null else StationTableReader.getStation(SUICA_BUS_STR, stationId,
+                Localizer.localizeString(R.string.suica_bus_area_line_stop,
+                        NumberUtils.intToHex(regionCode),
+                        NumberUtils.intToHex(lineCode),
+                        NumberUtils.intToHex(stationCode)))
+
+    }
+
+    /**
+     * Gets train station information from the Japan Rail (JR) table.
+     *
+     * @param regionCode  Train area/region ID (region code)
+     * @param lineCode    Train line ID (line code)
+     * @param stationCode Train station ID (station code)
+     * @return If the stop is known, a Station is returned describing it. If the stop is unknown,
+     * or there was some other database error, null is returned.
+     */
+    fun getRailStation(regionCode: Int, lineCode: Int, stationCode: Int): Station? {
+        var lineCode = lineCode
+        var stationCode = stationCode
+        val areaCode = regionCode shr 6 and 0xFF
+        lineCode = lineCode and 0xFF
+        stationCode = stationCode and 0xFF
+
+        val stationId = (areaCode shl 16) + (lineCode shl 8) + stationCode
+        return if (stationId == 0) null else StationTableReader.getStation(SUICA_RAIL_STR, stationId,
+                Localizer.localizeString(R.string.suica_area_line_station,
+                        NumberUtils.intToHex(regionCode),
+                        NumberUtils.intToHex(lineCode),
+                        NumberUtils.intToHex(stationCode)))
+
+    }
+}
