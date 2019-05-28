e29295c3febe3bc7824dac1fd47b7a691981ddc1
Michael Farrell <micolous+git@gmail.com>
2019-05-28 23:33:22 +0200

    krocap: Fixup copyright stanzas.

--- src/commonMain/kotlin/au/id/micolous/metrodroid/card/ksx6924/KROCAPData.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/card/ksx6924/KROCAPData.kt
@@ -1,3 +1,21 @@
+/*
+ * KROCAPData.kt
+ *
+ * Copyright 2019 Michael Farrell <micolous+git@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package au.id.micolous.metrodroid.card.ksx6924
 
 import au.id.micolous.metrodroid.card.iso7816.TagContents.DUMP_SHORT
@@ -5,8 +23,6 @@ import au.id.micolous.metrodroid.card.iso7816.TagContents.HIDE
 import au.id.micolous.metrodroid.card.iso7816.TagDesc
 
 object KROCAPData {
-    const val PROPRIETARY_TEMPLATE = "a5"
-
     // TODO: i18n
     val TAGMAP = mapOf(
             "50" to TagDesc("Payment terms", DUMP_SHORT),
