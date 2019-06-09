55b382f66b91fa3d1b5417015c45576231e9832e
Michael Farrell <micolous+git@gmail.com>
2019-06-09 18:02:27 +1000

    remove logging again, because that breaks travis

--- .travis/utils.sh
+++ .travis/utils.sh
@@ -6,10 +6,7 @@ SDKMANAGER="${ANDROID_SDK_ROOT}/tools/bin/sdkmanager"
 EMULATOR="${ANDROID_SDK_ROOT}/emulator/emulator"
 
 function android_install {
-    OUTF="$(mktemp)"
     echo "** Installing SDK package(s):" "$@"
-    echo y | ${SDKMANAGER} "$@" > "$OUTF"
-    RET=$?
-    tail "$OUTF"
-    return $RET
+    echo y | ${SDKMANAGER} "$@" > /dev/null
+    return $?
 }
