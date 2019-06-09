1c8f58c1bb02afd7b0b6cbe3de5bcd6865640f93
Michael Farrell <micolous+git@gmail.com>
2019-06-09 22:42:15 +1000

    test engine switch

--- .travis/install_android_emulator.sh
+++ .travis/install_android_emulator.sh
@@ -4,6 +4,7 @@
 source ./.travis/utils.sh
 
 EMULATOR_TARGET="system-images;android-${EMULATOR_API};default;${EMULATOR_ARCH}"
+EMULATOR_DIR="${ANDROID_SDK_ROOT}/system-images/android-${EMULATOR_API}/default/${EMULATOR_ARCH}"
 
 android_install \
     "platform-tools" \
@@ -16,6 +17,14 @@ $ADB start-server
 echo "** AVDs targets:"
 ${AVDMANAGER} list
 
+echo "** Possibly fixing broken images..."
+if ! [[ -e "${EMULATOR_DIR}/kernel-ranchu" ]]
+then
+    EMULATOR_ARGS="-engine classic"
+else
+    EMULATOR_ARGS=""
+fi
+
 echo "** Platform files:"
 ls -laR "${ANDROID_SDK_ROOT}/system-images"
 
@@ -24,7 +33,7 @@ echo "** Creating AVD for Android ${EMULATOR_API} on ${EMULATOR_ARCH}..."
 echo "no" | ${AVDMANAGER} create avd -n emu -k "${EMULATOR_TARGET}" -f
 
 echo "** Starting emulator in background..."
-${EMULATOR} -avd emu -no-skin -no-window &
+${EMULATOR} -avd emu -no-skin -no-window ${EMULATOR_ARGS} &
 
 echo "** Waiting for emulator..."
 ./.travis/android-wait-for-emulator
