bdc462efa36f232fe12cccb124d4a23a539543a8
Michael Farrell <micolous+git@gmail.com>
2019-06-09 21:23:28 +1000

    remove dead code

--- .travis/install_android_emulator.sh
+++ .travis/install_android_emulator.sh
@@ -4,7 +4,6 @@
 source ./.travis/utils.sh
 
 EMULATOR_TARGET="system-images;android-${EMULATOR_API};default;${EMULATOR_ARCH}"
-EMULATOR_DIR="${ANDROID_SDK_ROOT}/system-images/android-${EMULATOR_API}/default/${EMULATOR_ARCH}"
 
 android_install \
     "platform-tools" \
@@ -25,7 +24,7 @@ echo "** Creating AVD for Android ${EMULATOR_API} on ${EMULATOR_ARCH}..."
 echo "no" | ${AVDMANAGER} create avd -n emu -k "${EMULATOR_TARGET}" -f
 
 echo "** Starting emulator in background..."
-${EMULATOR} -avd emu -no-skin -no-window ${EMULATOR_ARGS} &
+${EMULATOR} -avd emu -no-skin -no-window &
 
 echo "** Waiting for emulator..."
 ./.travis/android-wait-for-emulator
