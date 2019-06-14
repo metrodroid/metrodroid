1d45dd6a4bdca43881359fb80ecbffa75a37001d
Vladimir Serbinenko <phcoder@google.com>
2019-06-14 03:39:46 +0200

    New card: Kazan transport card

--- src/commonMain/kotlin/au/id/micolous/metrodroid/card/classic/ClassicCardFactoryRegistry.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/card/classic/ClassicCardFactoryRegistry.kt
@@ -7,6 +7,7 @@ import au.id.micolous.metrodroid.transit.chc_metrocard.ChcMetrocardTransitData
 import au.id.micolous.metrodroid.transit.easycard.EasyCardTransitData
 import au.id.micolous.metrodroid.transit.erg.ErgTransitData
 import au.id.micolous.metrodroid.transit.gautrain.GautrainTransitFactory
+import au.id.micolous.metrodroid.transit.kazan.KazanTransitFactory
 import au.id.micolous.metrodroid.transit.kiev.KievTransitData
 import au.id.micolous.metrodroid.transit.komuterlink.KomuterLinkTransitFactory
 import au.id.micolous.metrodroid.transit.lax_tap.LaxTapTransitData
@@ -68,6 +69,7 @@ object ClassicCardFactoryRegistry {
             BonobusTransitFactory,
             GautrainTransitFactory(),
             MetroMoneyTransitFactory(),
+            KazanTransitFactory,
 
             // This check must be THIRD TO LAST.
             //
