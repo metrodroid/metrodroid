0b2a6121e2e184599fb63a36774af5eb5559b5ba
Vladimir Serbinenko <phcoder@google.com>
2019-08-07 19:52:00 +0200

    Mark HSL regions for localization and fix zero handling

--- src/commonMain/kotlin/au/id/micolous/metrodroid/transit/hsl/HSLLookup.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/hsl/HSLLookup.kt
@@ -41,52 +41,47 @@ object HSLLookup : En1545LookupUnknown() {
         2 -> Localizer.localizeString(R.string.hsl_english)
         else -> Localizer.localizeString(R.string.unknown_format, input.toString())
     }
+    
+    private val areaMap = mapOf(
+        Pair(0, 1) to R.string.hsl_region_helsinki,
+        Pair(0, 2) to R.string.hsl_region_espoo,
+        Pair(0, 4) to R.string.hsl_region_vantaa,
+        Pair(0, 5) to R.string.hsl_region_seutu,
+        Pair(0, 6) to R.string.hsl_region_kirkkonummi_siuntio,
+        Pair(0, 7) to R.string.hsl_region_vihti,
+        Pair(0, 8) to R.string.hsl_region_nurmijarvi,
+        Pair(0, 9) to R.string.hsl_region_kerava_sipoo_tuusula,
+        Pair(0, 10) to R.string.hsl_region_sipoo,
+        Pair(0, 14) to R.string.hsl_region_lahiseutu_2,
+        Pair(0, 15) to R.string.hsl_region_lahiseutu_3,
+        Pair(1, 1) to R.string.hsl_transport_bussi,
+        Pair(1, 2) to R.string.hsl_transport_bussi_2,
+        Pair(1, 3) to R.string.hsl_transport_bussi_3,
+        Pair(1, 4) to R.string.hsl_transport_bussi_4,
+        Pair(1, 5) to R.string.hsl_transport_raitiovaunu,
+        Pair(1, 6) to R.string.hsl_transport_metro,
+        Pair(1, 7) to R.string.hsl_transport_juna,
+        Pair(1, 8) to R.string.hsl_transport_lautta,
+        Pair(1, 9) to R.string.hsl_transport_u_linja
+    )
 
     fun getArea(parsed: En1545Parsed, prefix: String, isValidity: Boolean): String? {
-        val type = parsed.getInt(contractAreaTypeName(prefix))
-        val value = parsed.getInt(contractAreaName(prefix))
-        return when (type) {
-            // FIXME: i18n
-            null, 0 -> when (value) {
-                0 -> "Ei määritelty"
-                1 -> "Helsinki"
-                2 -> "Espoo"
-                4 -> "Vantaa"
-                5 -> "Seutu (HEL+ESP-VAN)"
-                6 -> "Kirkkonummi-Siuntio"
-                7 -> "Vihti"
-                8 -> "Nurmijärvi"
-                9 -> "Kerava-Sipoo-Tuusula"
-                10 -> "Sipoo"
-                14 -> "Lähiseutu 2 (ESP+VAN+KIR+KER+SIP)"
-                15 -> "Lähiseutu 3 (HEL+ESP+VAN+KIR+KER+SIP)"
-                null -> null
-                else -> Localizer.localizeString(R.string.unknown_format, "$type/$value")
-            }
-            1 -> when (value) {
-                0 -> "Ei määritelty"
-                1 -> "Bussi"
-                2 -> "Bussi 2"
-                3 -> "Bussi 3"
-                4 -> "Bussi 4"
-                5 -> "Raitiovaunu"
-                6 -> "Metro"
-                7 -> "Juna"
-                8 -> "Lautta"
-                9 -> "U-linja"
-                else -> Localizer.localizeString(R.string.unknown_format, "$type/$value")
-            }
-            2 -> {
-                value ?: return Localizer.localizeString(R.string.unknown_format, "$type/$value")
-                val to = value and 7
-                if (isValidity) {
-                    val from = value shr 3
-                    Localizer.localizePlural(R.plurals.hsl_zones, to - from + 1, String((from..to).map { 'A' + it }.toCharArray()))
-                } else {
-                    Localizer.localizeString(R.string.hsl_zone_station, String(charArrayOf('A' + to)))
-                }
-            }
-            else -> Localizer.localizeString(R.string.unknown_format, "$type/$value")
+        val type = parsed.getIntOrZero(contractAreaTypeName(prefix))
+        val value = parsed.getIntOrZero(contractAreaName(prefix))
+        if (type in 0..1 && value == 0) {
+            return null
         }
+        if (type == 2) {
+            val to = value and 7
+            if (isValidity) {
+                val from = value shr 3
+                return Localizer.localizePlural(R.plurals.hsl_zones, to - from + 1, String((from..to).map { 'A' + it }.toCharArray()))
+            } else {
+                return Localizer.localizeString(R.string.hsl_zone_station, String(charArrayOf('A' + to)))
+            }
+        }
+        return areaMap[Pair(type, value)]?.let {
+            Localizer.localizeString(it)
+        } ?: Localizer.localizeString(R.string.unknown_format, "$type/$value")
     }
 }
