e3d2c681064e0fea4569bfcedcd14b84be2a12ca
Vladimir Serbinenko <phcoder@google.com>
2019-08-11 19:26:19 +0200

    XmlOrJsonFormat: Simplify ZIP handling
    ZipIterator is clumsy, large and difficult to read. Instead replace it
    by way simpler direct iteration logic

--- src/jvmCommonMain/kotlin/au/id/micolous/metrodroid/serializers/XmlOrJsonFormat.kt
+++ src/jvmCommonMain/kotlin/au/id/micolous/metrodroid/serializers/XmlOrJsonFormat.kt
@@ -35,46 +35,24 @@ class XmlOrJsonCardFormat : CardImporter {
         when (peek(pb)) {
             '<' -> return iterateXmlCards(pb) { readCard(it) }
             '[', '{' -> return listOf(jsonKotlinFormat.readCard(pb.bufferedReader().readText())).iterator()
-            'P' -> return readZip(pb)
+            'P' -> return readZip(pb).iterator()
             else -> return null
         }
     }
 
-    class ZipIterator (private val zi: ZipInputStream)
-        : Iterator<Pair<ZipEntry, InputStream>> {
-        override fun next(): Pair<ZipEntry, InputStream> {
-            if (!advanced)
-                ze = zi.nextEntry
-            advanced = false
-            return Pair(ze!!, zi)
-        }
-
-        private var ze: ZipEntry? = null
-        private var end = false
-        private var advanced = false
-        override fun hasNext(): Boolean {
-            if (end)
-                return false
-            if (!advanced)
-                ze = zi.nextEntry
-            advanced = true
-            if (ze == null)
-                end = true
-            return !end
-        }
-    }
-
-    private fun readZip(stream: InputStream): Iterator<Card>? {
-        return IteratorTransformerNotNull(ZipIterator(ZipInputStream(stream))) {
-            (ze, zi) ->
+    private fun readZip(stream: InputStream): List<Card> {
+        val zi = ZipInputStream(stream)
+        val m = mutableListOf<Card>()
+        while (true) {
+            val ze = zi.nextEntry ?: break
             Log.d("Importer", "Importing ${ze.name}")
             when {
-                ze.name.endsWith(".json") -> jsonKotlinFormat.readCard(zi.bufferedReader().readText())
-                ze.name.endsWith(".xml") -> readCardXML(zi)
-                ze.name.endsWith(".mfc") -> mfcFormat.readCard(zi)
-                else -> null
+                ze.name.endsWith(".json") -> m += jsonKotlinFormat.readCard(zi.bufferedReader().readText())
+                ze.name.endsWith(".xml") -> m += readCardXML(zi)
+                ze.name.endsWith(".mfc") -> m += mfcFormat.readCard(zi)
             }
         }
+        return m
     }
 
     override fun readCard(stream: InputStream): Card {
