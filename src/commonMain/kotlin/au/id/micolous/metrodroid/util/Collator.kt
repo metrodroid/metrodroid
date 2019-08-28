61d5bdf76e3c5e01bda660c15c79452f1a0fd2d5
Vladimir Serbinenko <phcoder@google.com>
2019-08-28 20:03:09 +0200

    Add convenience function collatedBy

--- src/commonMain/kotlin/au/id/micolous/metrodroid/util/Collator.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/util/Collator.kt
@@ -3,3 +3,12 @@ package au.id.micolous.metrodroid.util
 expect object Collator {
     val collator: Comparator<in String>
 }
+
+inline fun <T> Iterable<T>.collatedBy(
+    crossinline selector: (T) -> String
+): List<T> {
+    val collator = Collator.collator
+    return this.sortedWith(Comparator { a, b ->
+        collator.compare(selector(a), selector(b))
+    })
+}
