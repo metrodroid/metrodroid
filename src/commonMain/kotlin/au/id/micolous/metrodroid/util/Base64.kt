8e68e6fb1dd8f536b230a802f270cbcb2e8a733b
Vladimir Serbinenko <phcoder@google.com>
2019-09-09 02:40:43 +0100

    Base64: Add missing SharedImmutable

--- src/commonMain/kotlin/au/id/micolous/metrodroid/util/Base64.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/util/Base64.kt
@@ -1,5 +1,7 @@
 package au.id.micolous.metrodroid.util
 
+import kotlin.native.concurrent.SharedImmutable
+
 /*
  * Base64 encoding/decoding (RFC1341)
  * Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
@@ -37,6 +39,7 @@ package au.id.micolous.metrodroid.util
  */
 
 private const val base64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+@SharedImmutable
 private val dtable = base64_table.mapIndexed { k, v -> v to k }.toMap() + mapOf('=' to 0)
 
 /**
