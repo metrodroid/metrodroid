8c566336cf88fdf7e4c8062e7fc47d9040480fd5
Vladimir Serbinenko <phcoder@google.com>
2019-09-15 22:32:48 +0200

    Fix copyright header coming from Apple template

--- native/metrodroid/metrodroidTests/metrodroidTests.swift
+++ native/metrodroid/metrodroidTests/metrodroidTests.swift
@@ -1,9 +1,20 @@
 //
 //  metrodroidTests.swift
-//  metrodroidTests
 //
-//  Created by Vladimir on 7/3/19.
-//  Copyright © 2019 Vladimir. All rights reserved.
+// Copyright 2019 Google
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import XCTest
@@ -12,23 +23,11 @@ import XCTest
 class metrodroidTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
