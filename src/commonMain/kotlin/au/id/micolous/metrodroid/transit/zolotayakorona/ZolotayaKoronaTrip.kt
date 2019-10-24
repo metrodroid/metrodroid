983f00f8e0df57bbaa42131e1a9735acd033b7f8
Vladimir Serbinenko <phcoder@google.com>
2019-10-24 15:38:10 +0200

    zolotayakorona: Show discount type
    In the same time show more raw fields

--- src/commonMain/kotlin/au/id/micolous/metrodroid/transit/zolotayakorona/ZolotayaKoronaTrip.kt
+++ src/commonMain/kotlin/au/id/micolous/metrodroid/transit/zolotayakorona/ZolotayaKoronaTrip.kt
@@ -21,8 +21,10 @@ package au.id.micolous.metrodroid.transit.zolotayakorona
 
 import au.id.micolous.metrodroid.multi.Parcelize
 import au.id.micolous.metrodroid.transit.TransitCurrency
+import au.id.micolous.metrodroid.transit.TransitData
 import au.id.micolous.metrodroid.transit.Trip
 import au.id.micolous.metrodroid.util.ImmutableByteArray
+import au.id.micolous.metrodroid.util.hexString
 
 private const val DEFAULT_FARE = 1300
 
@@ -33,7 +35,10 @@ internal data class ZolotayaKoronaTrip(private val mValidator: String,
         // sequential number of round trips that bus makes
                                        private val mTrackNumber: Int,
                                        private val mPreviousBalance: Int,
-                                       private val mNextBalance: Int?) : Trip() {
+                                       private val mNextBalance: Int?,
+                                       private val mA: Int,
+                                       private val mB: Int,
+                                       private val mC: Int) : Trip() {
     private val estimatedFare
         get() = when (mCardType) {
             0x760500 -> 1150
@@ -59,6 +64,9 @@ internal data class ZolotayaKoronaTrip(private val mValidator: String,
 
     override val mode get() = Trip.Mode.BUS
 
+    override fun getRawFields(level: TransitData.RawLevel): String? = "A=${mA.hexString}/B=${mB.hexString}/C=${mC.hexString}" +
+            if (level == TransitData.RawLevel.ALL) "/trackNumber=$mTrackNumber/previousBalance=$mPreviousBalance" else ""
+
     companion object {
         fun parse(block: ImmutableByteArray, cardType: Int, refill: ZolotayaKoronaRefill?, balance: Int?): ZolotayaKoronaTrip? {
             if (block.isAllZero())
@@ -71,10 +79,13 @@ internal data class ZolotayaKoronaTrip(private val mValidator: String,
                     balanceAfter -= refill.mAmount
             }
             return ZolotayaKoronaTrip(
+                    mA = block.byteArrayToInt(0, 2),
                     mValidator = block.getHexString(2, 3),
+                    mB = block[5].toInt() and 0xff,
                     mTime = time,
                     mTrackNumber = block.byteArrayToInt(10, 1),
                     mPreviousBalance = block.byteArrayToIntReversed(11, 4),
+                    mC = block[15].toInt() and 0xff,
                     mNextBalance = balanceAfter,
                     mCardType = cardType
             )
