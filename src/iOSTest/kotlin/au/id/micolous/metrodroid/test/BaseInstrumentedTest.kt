4ab7cf9929b010d1a75e8d43ac02ed6395e8007c
Vladimir Serbinenko <phcoder@gmail.com>
2021-10-14 22:23:27 +0200

    Make iOSSimulatorTest work

---
+++ src/iOSTest/kotlin/au/id/micolous/metrodroid/test/BaseInstrumentedTest.kt
@@ -0,0 +1,29 @@
+package au.id.micolous.metrodroid.test
+
+import au.id.micolous.metrodroid.util.ConcurrentFileReader
+import au.id.micolous.metrodroid.util.Input
+import au.id.micolous.metrodroid.util.Preferences
+import kotlinx.coroutines.runBlocking
+import platform.Foundation.NSBundle
+import platform.Foundation.NSError
+import platform.Foundation.NSFileManager
+
+actual fun <T> runAsync(block: suspend () -> T) {
+    runBlocking { block() }
+}
+
+actual abstract class BaseInstrumentedTestPlatform actual constructor() {
+    actual fun setLocale(languageTag: String) {
+        // Preferences.languageActual = languageTag.substringBefore("-")
+        //Locale.setDefault(Locale.forLanguageTag(languageTag)) TODO
+    }
+
+    actual fun loadAssetSafe(path: String): Input? =
+        ConcurrentFileReader.openFile(fullPathForAsset(path))?.makeInput()
+
+    actual fun listAsset(path: String): List<String>? =
+        NSFileManager.defaultManager.contentsOfDirectoryAtPath(fullPathForAsset(path), null)
+            ?.filterIsInstance<String>()
+
+    private fun fullPathForAsset(path: String): String = NSBundle.mainBundle.resourcePath + "/$path"
+}
