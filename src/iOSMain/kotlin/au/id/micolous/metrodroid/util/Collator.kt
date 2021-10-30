3a29b6f78f94f134c4c5a058b09eb6047d3c4c61
Vladimir <phcoder@gmail.com>
2021-10-30 14:18:22 -0400

    Silence spurious warning

--- src/iOSMain/kotlin/au/id/micolous/metrodroid/util/Collator.kt
+++ src/iOSMain/kotlin/au/id/micolous/metrodroid/util/Collator.kt
@@ -3,6 +3,7 @@ package au.id.micolous.metrodroid.util
 import platform.Foundation.*
 
 object StandardComparator : Comparator<String> {
+    @Suppress("CAST_NEVER_SUCCEEDS") // Spurious warning
     override fun compare(a: String, b: String): Int = (a as NSString).localizedStandardCompare(b).toInt()
 }
 
